shiptest-ss13/Shiptest

Wallets and Bank Cards (#2979)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->
<!-- You can view Contributing.MD for a detailed description of the pull
request process. -->

## About The Pull Request

Makes wallets part of the outfit shit and splits money behavior of
access cards into bank cards
<!-- Describe The Pull Request. Please be sure every change is
documented or this can delay review and even discourage maintainers from
merging your PR! -->

## Why It's Good For The Game

<!-- Please add a short description of why you think these changes would
benefit the game. If you can't justify it in words, it might not be
worth adding. -->

## Changelog

:cl:
add: Bank accounts are now handled through cash cards! You now spawn
with a wallet to store your extra card!
/:cl:

<!-- Both :cl:'s are required for the changelog to work! You can put
your name to the right of the first :cl: if you want to overwrite your
GitHub username as author ingame. -->
<!-- You can use multiple of the same prefix (they're only used for the
icon ingame) and delete the unneeded ones. Despite some of the tags,
changelogs should generally represent how a player might be affected by
the changes rather than a summary of the PR's contents. -->

code/__DEFINES/is_helpers.dm
@@ -176,6 +176,8 @@ GLOBAL_LIST_INIT(turfs_without_ground, typecacheof(list(
 
 #define isidcard(I) (istype(I, /obj/item/card/id))
 
+#define isbankcard(I) (istype(I, /obj/item/card/bank))
+
 #define isstructure(A) (istype(A, /obj/structure))
 
 #define ismachinery(A) (istype(A, /obj/machinery))

code/game/objects/structures/displaycase.dm
@@ -422,7 +422,7 @@
 	. = ..()
 	if(.)
 		return
-	var/obj/item/card/id/potential_acc = usr.get_idcard(hand_first = TRUE)
+	var/obj/item/card/bank/potential_acc = usr.get_bankcard()
 	switch(action)
 		if("Buy")
 			if(!showpiece)
@@ -497,9 +497,9 @@
 			return TRUE
 	. = TRUE
 /obj/structure/displaycase/forsale/attackby(obj/item/I, mob/living/user, params)
-	if(isidcard(I))
+	if(isbankcard(I))
 		//Card Registration
-		var/obj/item/card/id/potential_acc = I
+		var/obj/item/card/bank/potential_acc = I
 		if(!potential_acc.registered_account)
 			to_chat(user, "<span class='warning'>This ID card has no account registered!</span>")
 			return

code/modules/modular_computers/laptop_vendor.dm
@@ -248,10 +248,10 @@
 		visible_message("<span class='info'>[user] inserts a [HC.credits] cr holocredit chip into [src].</span>")
 		qdel(HC)
 		return
-	else if(istype(I, /obj/item/card/id))
+	else if(istype(I, /obj/item/card/bank))
 		if(state != 2)
 			return
-		var/obj/item/card/id/ID = I
+		var/obj/item/card/bank/ID = I
 		var/datum/bank_account/account = ID.registered_account
 		var/target_credits = total_price - credits
 		if(!account.adjust_money(-target_credits, "laptop_vendor"))

ApocryphaXIII/ApocryphaXIII

code/__DEFINES/is_helpers.dm
@@ -196,6 +196,8 @@ GLOBAL_LIST_INIT(turfs_without_ground, typecacheof(list(
 
 #define isidcard(I) (istype(I, /obj/item/card/id))
 
+#define isbankcard(I) (istype(I, /obj/item/vamp/creditcard))
+
 #define isstructure(A) (istype(A, /obj/structure))
 
 #define ismachinery(A) (istype(A, /obj/machinery))

code/game/objects/structures/displaycase.dm
@@ -441,10 +441,7 @@
 	. = ..()
 	if(.)
 		return
-	var/obj/item/card/id/potential_acc
-	if(isliving(usr))
-		var/mob/living/L = usr
-		potential_acc = L.get_idcard(hand_first = TRUE)
+	var/obj/item/vamp/creditcard/potential_acc = usr.get_bankcard()
 	switch(action)
 		if("Buy")
 			if(!showpiece)
@@ -520,9 +517,9 @@
 			return TRUE
 	. = TRUE
 /obj/structure/displaycase/forsale/attackby(obj/item/I, mob/living/user, params)
-	if(isidcard(I))
+	if(isbankcard(I))
 		//Card Registration
-		var/obj/item/card/id/potential_acc = I
+		var/obj/item/vamp/creditcard/potential_acc = I
 		if(!potential_acc.registered_account)
 			to_chat(user, "<span class='warning'>This ID card has no account registered!</span>")
 			return

code/modules/modular_computers/laptop_vendor.dm
@@ -250,10 +250,10 @@
 		visible_message("<span class='info'>[user] inserts a [HC.get_item_credit_value()] cr holocredit chip into [src].</span>")
 		qdel(HC)
 		return
-	else if(istype(I, /obj/item/card/id))
+	else if(istype(I, /obj/item/vamp/creditcard))
 		if(state != 2)
 			return
-		var/obj/item/card/id/ID = I
+		var/obj/item/vamp/creditcard/ID = I
 		var/datum/bank_account/account = ID.registered_account
 		var/target_credits = total_price - credits
 		if(!account.adjust_money(-target_credits))
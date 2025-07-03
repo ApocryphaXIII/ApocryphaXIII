700a7aa178a82ecf974369e5c6cbe1ba32eaca3d
FalloutFalcon <falloutfalconplays@gmail.com>
2025-07-03 02:12:45 -0500

    readme tweaks

--- tgui/packages/tgui/interfaces/Changelog.jsx
+++ tgui/packages/tgui/interfaces/Changelog.jsx
@@ -189,7 +189,7 @@ export class Changelog extends Component {
         <h1>Apocrypha: SS13</h1>
         <p>
           <b>Thanks to: </b>
-          The Final Nights, World of Darkness 13, TGStation, Baystation 12, /vg/station, NTstation, CDK Station devs,
+          The Final Nights, World of Darkness 13, RequiemSS13, TGStation, Baystation 12, /vg/station, NTstation, CDK Station devs,
           FacepunchStation, GoonStation devs, the original Space Station 13
           developers, Invisty for the title image and the countless others who
           have contributed to the game, issue tracker or wiki over the years.
